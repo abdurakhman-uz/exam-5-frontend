4b5d048daeeeeaf3f3289836128c43d343c931c7
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-04 20:04:43 +0500

    fixed Card image covering

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ data, cars }) => {
         style={{ position: "relative" }}
       >
         <div key={data._id} className={`w-[240px] h-[300px] bg-gray-100 rounded-xl relative flex justify-center`}>
-          <img src={`${import.meta.env.VITE_BECKEND}/api/images/${data.image}`} className={`w-[210px] rounded-xl h-[180px] absolute top-4`} />
+          <img src={`${import.meta.env.VITE_BECKEND}/api/images/${data.image}`} className={`w-[210px] rounded-xl h-[180px] absolute top-4 object-cover`} />
           <h1 className={`absolute top-[220px] text-xl`}>{data.name}</h1>
         </div>
       </Link>
