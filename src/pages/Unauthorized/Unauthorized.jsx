10cf9b2db6ba5267430897810c7cfc1f9c68f4ed
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-04 22:15:58 +0500

    updated status page

--- src/pages/Unauthorized/Unauthorized.jsx
+++ src/pages/Unauthorized/Unauthorized.jsx
@@ -5,8 +5,8 @@ const UnauthorizedPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <div className="max-w-md bg-white shadow-lg rounded-lg p-6">
-                <h1 className="text-4xl font-bold text-gray-800 mb-8">401</h1>
-                <p className="text-gray-600 mb-4">
+                <h1 className="text-4xl font-bold text-gray-800 mb-4">401</h1>
+                <p className="text-gray-600 mb-8">
                     Oops! You are not authorized to access this page.
                 </p>
                 <Link to="/" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
