c013985d9a9653e16295d6496e4b3fe3f031056f
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-04 22:00:52 +0500

    replaced button to Link

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="max-w-md bg-white shadow-lg rounded-lg p-6">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
-        <p className="text-gray-600 mb-4">
+        <p className="text-gray-600 mb-8">
           Oops! The page you're looking for could not be found.
         </p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+        <Link to="/" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
           Go Back to Homepage
-        </button>
+        </Link>
       </div>
     </div>
   );
