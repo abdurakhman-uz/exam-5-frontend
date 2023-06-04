5948a241670f338712f3d37436fe0ce7c9bdc201
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-04 20:03:12 +0500

    added Login button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Card from "../../components/Card/Card";
 import css from "./Home.module.css";
 
@@ -12,6 +13,7 @@ const Home = () => {
 
   return (
     <>
+      <Link to="/login" className=" absolute top-4 right-4 text-white text-lg py-2 px-4 bg-[#4096ff] rounded-lg">Login</Link>
       <div className={`${css.container} flex mt-20`}>
         <div className={`${css.wrapper}`}>
           {
