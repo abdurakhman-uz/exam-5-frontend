d95af04d7f33931c62505fbbe599d81c28ca7354
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-04 22:06:08 +0500

    created Unauthorized page

---
+++ src/pages/Unauthorized/Unauthorized.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const UnauthorizedPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <div className="max-w-md bg-white shadow-lg rounded-lg p-6">
+                <h1 className="text-4xl font-bold text-gray-800 mb-8">401</h1>
+                <p className="text-gray-600 mb-4">
+                    Oops! You are not authorized to access this page.
+                </p>
+                <Link to="/" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                    Go Back to Homepage
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default UnauthorizedPage;
