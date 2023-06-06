dd85ca24986b6310d81a4e63910ef4f47f700b38
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-06 13:52:34 +0500

    Basked updated

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,41 +1,85 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const BasketPage = () => {
-  return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-      {/* Header */}
-      <header className="bg-white py-4 px-8 shadow">
-        <h1 className="text-2xl font-bold text-gray-800">Shopping Basket</h1>
-      </header>
+    const token = localStorage.getItem("token")
+    const backend = import.meta.env.VITE_BECKEND
+    const [data, setData] = useState([])
+    const navigate = useNavigate()
 
-      {/* Main Content */}
-      <main className="flex-1 p-8">
-        <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-6">
-          {/* Basket items */}
-          <h2 className="text-xl font-semibold mb-4">Items in Your Basket</h2>
-          {/* Add your basket item components or content here */}
+    useEffect(() => {
+        fetch(backend + '/api/user/savat/', {
+            method: 'GET',
+            headers: {
+                token: token
+            }
+        })
+            .then(res => {
+                if (res.status === 403 || res.status === 401) {
+                    navigate('/login')
+                    return
+                }
+                return res.json()
+            })
+            .then(data => {
+                setData(data.msg)
+            })
+    })
 
-          {/* Basket summary */}
-          <div className="mt-8">
-            <h2 className="text-xl font-semibold mb-4">Basket Summary</h2>
-            {/* Add your basket summary components or content here */}
-          </div>
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-100">
+            {/* Header */}
+            <header className="bg-white py-4 px-8 shadow">
+                <h1 className="text-2xl font-bold text-gray-800">Shopping Basket</h1>
+            </header>
 
-          {/* Checkout button */}
-          <div className="mt-8 flex justify-end">
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
-              Proceed to Checkout
-            </button>
-          </div>
+            {/* Main Content */}
+            <main className="flex-1 p-8">
+                <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-6">
+                    {/* Basket items */}
+                    <h2 className="text-xl font-semibold mb-4">Items in Your Basket</h2>
+                    <ul>
+                        {
+                            data[0] ?
+                                data?.map(data => (
+                                    <li key={data._id} className="w-[100%] flex justify-around">
+                                        <p>{data.name}</p>
+                                        <p>{data.gearbook}</p>
+                                        <p>{data.motor}</p>
+                                        <p>{data.distance}</p>
+                                        <p>{data.year}</p>
+                                        <p>{data.price}</p>
+                                    </li>
+                                ))
+                                :
+                                <>
+                                    <h1>Nothing Found</h1>
+                                </>
+                        }
+                    </ul>
+
+                    {/* Basket summary */}
+                    <div className="mt-8">
+                        <h2 className="text-xl font-semibold mb-4">Basket Summary</h2>
+                        <p>TOTAL: {data.reduce((a, b) => a + parseInt(b.price), 0)}$</p>
+                    </div>
+
+                    {/* Checkout button */}
+                    <div className="mt-8 flex justify-end">
+                        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                            Proceed to Checkout
+                        </button>
+                    </div>
+                </div>
+            </main>
+
+            {/* Footer */}
+            <footer className="bg-white py-4 px-8 shadow">
+                <p className="text-gray-600">© 2023 My Store. All rights reserved.</p>
+            </footer>
         </div>
-      </main>
-
-      {/* Footer */}
-      <footer className="bg-white py-4 px-8 shadow">
-        <p className="text-gray-600">© 2023 My Store. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+    );
 };
 
 export default BasketPage;
