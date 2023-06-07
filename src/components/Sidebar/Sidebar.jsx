74fb7f1290265890b15a47279782cacc19198240
abdurakhman-uz <rockbizzy667@yandex.kz>
2023-06-07 10:45:22 +0500

    added Models Link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,9 +5,9 @@ const Sidebar = () => {
   return (
     <div className='w-[200px] h-[100vh] bg-white'>
         <div className='relative top-10 h-[200px] flex flex-col justify-evenly items-center text-lg'>
+            <Link to="/private/admin/models">Modellar</Link>
             <Link to="/private/admin/products">Asosiy</Link>
             <Link to="/private/admin/users">Users</Link>
-            <Link to="#">E'lonlar</Link>
             <Link to="#">Savollar</Link>
         </div>
     </div>
